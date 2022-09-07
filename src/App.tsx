e1e61633bf76252e67c91b3b089d085fdefb48bd
Nekit678 <70006226+Nekit678@users.noreply.github.com>
2022-09-08 03:06:14 +0600

    Adding AntDesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,41 @@
 import './App.css';
 import Preloader from './components/common/Preloader/Preloader';
-import Header from './components/Header/Header';
+import 'antd/dist/antd.css';
+
+import type { MenuProps } from 'antd';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import Image from 'antd/lib/image';
 import Navbar from './components/Navbar/Navbar';
+
+const { Header, Content, Footer} = Layout;
 
 //приложение
 function App() {
+
   return (
-    <div className="app-wrapper">
-      <Header></Header>
+    <Layout style={{ minHeight: '100vh' }}>
       <Navbar></Navbar>
-      <div className="app-wrapper-content">
-        <Preloader></Preloader>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-        <div>TEst</div>
-      </div>
-      
-    </div>
+      <Layout className="site-layout">
+        
+        
+        <Content style={{ margin: '0 16px' }}>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+            <Preloader></Preloader>
+          </div>
+        </Content>
+
+        <Footer style={{ textAlign: 'center' }}>UnibotPortal ©2022 Created by JustLena</Footer>
+
+      </Layout>
+    </Layout>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,55 @@
 import s from './Navbar.module.css'
+import { useState } from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import {Layout, Menu, Image } from 'antd'
+
+const {Sider} = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+];
 
 function Navbar() {
+    const [collapsed, setCollapsed] = useState(false);
+
     return (
-        <nav className={s.nav}>
-            <div>Test</div>
-            <div>Test</div>
-            <div>Test</div>
-            <div>Test</div>
-            <div>Test</div>
-            <div>Test</div>
-        </nav>
+        <Sider collapsed={collapsed} onMouseOver = {() => setCollapsed(false)} onMouseOut = {() => setCollapsed(true)}>
+            <div className={s.logo}>
+                <Image src='https://www.codewars.com/packs/assets/logo.61192cf7.svg'></Image>
+            </div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        </Sider>
     )
 }
 
